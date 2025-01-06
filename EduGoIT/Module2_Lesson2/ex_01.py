4171e6077cf55c28441479dd35be0196cc9e4006
ViktoriiaTrachuk <trachukvic@gmail.com>
2025-01-06 17:37:40 +0100

    adding new exercises

---
+++ EduGoIT/Module2_Lesson2/ex_01.py
@@ -0,0 +1,7 @@
+a = [2, 4, 5, 8, 3]
+k = 0
+for b in a:
+    if k < b:
+        k = b
+print(k)
+
---
+++ EduGoIT/Module2_Lesson2/ex_03.py
@@ -0,0 +1,36 @@
+a = "120"
+a = int(a)
+if a > 0:
+    print(f'a - "positive')
+elif a < 0:
+    print("negative")
+else:
+    print("it is zero")
+
+# a = "12.0"
+# a = int(a)
+# if a > 0:
+#     print(f'a - "positive')
+# elif a < 0:
+#     print("negative")
+# else:
+#     print("it is zero")
+
+a = "12,0"
+a = float(a)
+if a > 0:
+    print(f'a - "positive')
+elif a < 0:
+    print("negative")
+else:
+    print("it is zero")
+
+b = "12.0"
+b = float(b)
+if b > 0:
+    print(f'{b} - positive')
+elif b < 0:
+    print("negative")
+else:
+    print("it is zero")
+
